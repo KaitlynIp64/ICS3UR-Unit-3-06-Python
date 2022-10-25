c830cc868f8264c2cf5555ef4956f9a1ef515f9b
Kaitlyn Ip <kaitlyn.ip@mths.ca>
2022-10-25 20:19:56 +0000

    fixed code

--- number_guessing.py
+++ number_guessing.py
@@ -15,15 +15,16 @@ def main():
     random_variable = random.randint(1, 9)  # a number between 1 and 9
 
     # process & output
-
     try:
-        integer_as_number = int(user_guess)
+        user_guess = int(user_guess)
         print("You entered an integer correctly.")
+
     except ValueError:
         print("That was not an integer.")
+
+    else:
         if user_guess == random_variable:
             print("You guessed the correct number!")
-
         else:
             print(
                 "You guessed the wrong number! The number was "
